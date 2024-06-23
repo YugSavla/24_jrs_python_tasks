3be33d8f9f86f2042186c2944f08b3d537b91b1d
Yug Savla <yugsavla1@gmail.com>
2024-06-23 15:35:05 +0530

    Update solution.py

--- lec_1/solution.py
+++ lec_1/solution.py
@@ -1,1 +1,16 @@
 #write your answer here
+#Yug Savla
+1. LeetCode Solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        n=len(nums)
+        for i in range(0, n):
+            for j in range(i+1, n):
+                if(nums[i]+nums[j]==target):
+                    return [i,j]
+        return []
+2.  Printing 1 2 4 8 other than variable addition
+a = 1
+for i in range(4):
+    print(a)
+    a = a << 1
